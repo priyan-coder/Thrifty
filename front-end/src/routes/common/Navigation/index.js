1715f9c092133a27d8658181c7f807b045a6777b
Priyan Rajamohan <rajamohanpriyan@gmail.com>
2022-10-20 16:47:40 -0400

    Modified styles for navigation

--- front-end/src/routes/common/Navigation/index.js
+++ front-end/src/routes/common/Navigation/index.js
@@ -1,23 +1,51 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
+import styled from 'styled-components';
 // Outlet renders the nested components between the <Route></Route>
 // Navigation Bar Persists for all routes
 const Navigation = () => {
   return (
     <Fragment>
-      <div>
-        <Link to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrwnLogo />
-        </Link>
-        <div>
-          <Link to="/fav">Favorites</Link>
-          <Link to="/cart">Cart</Link>
-        </div>
-      </div>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/fav">Favorites</NavLink>
+          <NavLink to="/cart">Cart</NavLink>
+        </NavLinks>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
 };
+
+export const NavigationContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 70px;
+  padding: 25px;
+`;
+
+export const NavLinks = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const NavLink = styled(Link)`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
 
 export default Navigation;
