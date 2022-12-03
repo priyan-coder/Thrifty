85c3d7ce88e67b101ac13b975e864a27be7a7335
Priyan Rajamohan <rajamohanpriyan@gmail.com>
2022-12-03 04:52:40 -0500

    added search functionality to the search component

--- front-end/src/routes/buyer/ProductPage/index.js
+++ front-end/src/routes/buyer/ProductPage/index.js
@@ -2,22 +2,49 @@ import ProductCard from '../../../components/ProductCard';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { allProducts } from '../../../mockData/MockData';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import TextField from '@mui/material/TextField';
 const ProductPage = () => {
   const { category } = useParams();
   const products = allProducts[category];
+  const [searchInput, setSearchInput] = useState('');
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
+      <SearchBarContainer>
+        <TextField
+          sx={{ minWidth: '70%' }}
+          name="searchInput"
+          type="text"
+          value={searchInput}
+          id="searchInput"
+          label="Search from chosen category"
+          variant="outlined"
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+      </SearchBarContainer>
+
       <CategoryContainer>
-        {products &&
-          products.map((product) => (
+        {filteredProducts &&
+          filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
       </CategoryContainer>
     </Fragment>
   );
 };
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 
 const CategoryContainer = styled.div`
   display: grid;
