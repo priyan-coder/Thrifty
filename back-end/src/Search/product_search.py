cda1c0a4f72b82536c1f106e6e211ea545d37f38
AnsuhCase <112497948+AnsuhCase@users.noreply.github.com>
2022-12-07 21:29:06 -0500

    added projections

--- back-end/src/Search/product_search.py
+++ back-end/src/Search/product_search.py
@@ -7,8 +7,8 @@ class Search():
     def get_search_results(category):
         if category is not None:
             query = {"category" : category}
-
-            docs = MongoUtils.get_docs_from_mongo(query=query, collection_name="products",db_name="thriftstore")
+            projections = {"_id" : 0}
+            docs = MongoUtils.get_docs_from_mongo(query=query, projections=projections,collection_name="products",db_name="thriftstore")
 
             return {"response" : docs}
 
